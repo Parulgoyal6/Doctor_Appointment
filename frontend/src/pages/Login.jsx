9ef3893ea6d0e47a9ba0afc1e45247a8e76a744c
Parul Goyal <149240936+Parulgoyal6@users.noreply.github.com>
2024-10-28 12:17:04 +0530

    Update Login.jsx

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,6 +1,7 @@
 import React, { useContext, useEffect, useState } from 'react'
 import { AppContext } from '../context/AppContext'
 import {useNavigate} from 'react-router-dom'
+import { toast } from 'react-toastify';
 import axios from 'axios'
 const Login = () => {
 
